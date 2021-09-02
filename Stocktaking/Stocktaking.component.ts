9f068d2876aae420401a678d31b9d7d10c4dd485
Gugu22 <66216439+Gugu22@users.noreply.github.com>
2021-09-02 07:26:04 +0200

    Add files via upload

---
+++ Stocktaking/Stocktaking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Stocktaking',
+  templateUrl: './Stocktaking.component.html',
+  styleUrls: ['./Stocktaking.component.css']
+})
+export class StocktakingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
